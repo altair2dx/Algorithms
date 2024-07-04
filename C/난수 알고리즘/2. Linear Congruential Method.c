ba4fac29bfa95ce32734d13be7d59aed79a8ddc2
altair2dx <167539294+altair2dx@users.noreply.github.com>
2024-07-04 14:27:02 +0000

    Updated

--- C/난수 알고리즘/2. Linear Congruential Method.c
+++ C/난수 알고리즘/2. Linear Congruential Method.c
@@ -17,17 +17,20 @@
 #include <stdio.h>
 #include <time.h>
 
+#define A 1103515245
+#define C 12345
+#define M 0x7fffffff
+
 // LCG 함수
 int LCG(unsigned int seed) {
-    return ((seed * 1103515245) + 12345) & 0x7fffffff;
+    return ((seed * A) + C) & M;
 }
 
 int main() {
-    // 현재 시간을 시드로 하여 난수생성
-    unsigned int seed = time(NULL);
+    unsigned int seed = time(NULL); // 현재 시간을 시드로 설정
     for(int i = 0; i < 4; ++i) {
-        seed = LCG(seed);
-        double result = (double)seed / 0x7fffffff; 
+        seed = LCG(seed); // 난수 생성
+        double result = (double)seed / M; // 난수를 [0,1] 범위로 정규화
         printf("%lf \n", result);
     }
     
