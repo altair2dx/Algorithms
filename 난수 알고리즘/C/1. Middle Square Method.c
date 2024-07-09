16ead8238c914cedf973168de3bbc505ddde724f
altair2dx <167539294+altair2dx@users.noreply.github.com>
2024-07-09 14:58:30 +0000

    Updated

--- 난수 알고리즘/C/1. Middle Square Method.c
+++ 난수 알고리즘/C/1. Middle Square Method.c
@@ -1,17 +1,5 @@
-/* --- 중앙제곱법 (Middle Square Method) --- */
-// 수열 Xₙ₊₁ = {(Xₙ)² 중앙의 a자리} 를 이용해 난수를 생성하는 방식.
-// 1949년 고안, 난수의 품질이 좋지 않아 현재는 사용하지 않는다.
-
-// 예시
-// X₀ = 1773, a = 4
-// X₀² = 3143529 → X₁ = 1435
-// X₁² = 2059225 → X₂ = 592
-// X₂² = 350464 → X₃ = 5046
-// X₃² = 25462116 → X₄ = 4621
-// ...
-
-// 코드
 #include <stdio.h>
+#include <time.h>
 #include <math.h>
 
 int numDigits(unsigned long long number) {
@@ -26,11 +14,12 @@ unsigned long long middleSqaure (unsigned long long seed, unsigned long long dig
 }
 
 int main() {
-    unsigned long long seed = 1773;
-    unsigned long long digit = 4;
-    for (int i = 0; i<5; i++) {
-        printf("X%d = %llu \n", i, seed);
-        seed = middleSqaure(seed, digit);
+    unsigned long long seed = time(NULL);
+    unsigned long long digit = 6;
+    unsigned long long ms = middleSqaure(seed, digit); 
+    for (int i = 1; i<101; i++) {
+        printf("X%d = %llu \n", i, ms);
+        ms = middleSqaure(ms, digit);
     }
 
     return 0;
